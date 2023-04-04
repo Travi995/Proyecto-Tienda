0b74d36cfede6aa33501faf4a5a29bb65083b572
Travi995 <joseantoniotravieso259@gmail.com>
2023-04-04 16:00:59 -0400

    JTB: Fondo nuevo

--- public/src/js/vendedor.js
+++ public/src/js/vendedor.js
@@ -1,1 +1,10 @@
- 
+
+
+function Cambio(){
+    window.location.href = "./../html/producto.html"
+}
+function Atras (){
+    window.location.href = "./../html/seleccion.html"
+}
+document.getElementById("adelante").onclick = Cambio
+document.getElementById("atras").onclick = Atras
