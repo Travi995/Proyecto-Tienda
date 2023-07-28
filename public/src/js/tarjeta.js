7be2cd5f42850f814e28745d3e855ac918abd535
Travi995 <joseantoniotravieso259@gmail.com>
2023-07-28 09:24:19 -0400

    chore(JTB/Front): diseno de las tarjetas de productos finalizado

---
+++ public/src/js/tarjeta.js
@@ -0,0 +1,22 @@
+
+
+const enlaceCompra = () => {
+    let elementos = document.getElementsByClassName('linkWhats')
+    
+    
+
+
+
+
+
+
+
+
+    elementos.forEach(element => {
+        element.addEventlistenner('click', () => {
+            
+        })
+    });
+
+}
+
